38fc34a6981895318b869da73e3daafa315a16bb
Vitaliy Sulimenko <prevetal@gmail.com>
2023-08-30 18:17:27 +0200

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,198 @@
+// Ширина окна для ресайза
+WW = window.innerWidth || document.clientWidth || document.getElementsByTagName('body')[0].clientWidth
+WH = window.innerHeight || document.clientHeight || document.getElementsByTagName('body')[0].clientHeight
+BODY = document.getElementsByTagName('body')[0]
+OVERLAY = document.querySelector('.overlay')
+
+
+document.addEventListener('DOMContentLoaded', function () {
+	// Основной слайдер на главной
+	let mainSlider = document.querySelector('.main_slider .swiper')
+
+	if (mainSlider) {
+		new Swiper('.main_slider .swiper', {
+			loop: true,
+			speed: 750,
+			watchSlidesProgress: true,
+			slideActiveClass: 'active',
+			slideVisibleClass: 'visible',
+			spaceBetween: 0,
+			slidesPerView: 1,
+			pagination: {
+				el: '.swiper-pagination',
+				type: 'bullets',
+				clickable: true,
+				bulletActiveClass: 'active'
+			},
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev'
+			},
+			preloadImages: false,
+			lazy: {
+				enabled: true,
+				checkInView: true,
+				loadOnTransitionStart: true,
+				loadPrevNext: true
+			}
+		})
+	}
+
+
+	// Наше оборудование
+	const equipmentSliders = [],
+		equipment = document.querySelectorAll('.equipment .swiper')
+
+	equipment.forEach(function (el, i) {
+		el.classList.add('equipment_s' + i)
+
+		let options = {
+			loop: false,
+			speed: 500,
+			watchSlidesProgress: true,
+			slideActiveClass: 'active',
+			slideVisibleClass: 'visible',
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev'
+			},
+			pagination: {
+				el: '.swiper-pagination',
+				type: 'bullets',
+				clickable: true,
+				bulletActiveClass: 'active'
+			},
+			spaceBetween: 24,
+			slidesPerView: 1
+		}
+
+		equipmentSliders.push(new Swiper('.equipment_s' + i, options))
+	})
+
+
+	// Табы
+	var locationHash = window.location.hash
+
+	$('body').on('click', '.tabs .btn', function(e) {
+		e.preventDefault()
+
+		if (!$(this).hasClass('active')) {
+			const $parent           = $(this).closest('.tabs_container'),
+				  activeTab         = $(this).data('content'),
+				  $activeTabContent = $(activeTab),
+				  level             = $(this).data('level')
+
+			$parent.find('.tabs:first .btn').removeClass('active')
+			$parent.find('.tab_content.' + level).removeClass('active')
+
+			$(this).addClass('active')
+			$activeTabContent.addClass('active')
+		}
+	})
+
+	if (locationHash && $('.tabs_container').length) {
+		const $activeTab        = $(`.tabs .btn[data-content="${locationHash}"]`),
+			  $activeTabContent = $(locationHash),
+			  $parent           = $activeTab.closest('.tabs_container'),
+			  level             = $activeTab.data('level')
+
+		$parent.find('.tabs:first .btn').removeClass('active')
+		$parent.find('.tab_content.' + level).removeClass('active')
+
+		$activeTab.addClass('active')
+		$activeTabContent.addClass('active')
+
+		$('html, body').stop().animate({ scrollTop: $activeTabContent.offset().top }, 1000)
+	}
+
+
+	// Плавная прокрутка к якорю
+	const scrollBtns = document.querySelectorAll('.scroll_btn')
+
+	if (scrollBtns) {
+		scrollBtns.forEach(element => {
+			element.addEventListener('click', e => {
+				e.preventDefault()
+
+				let anchor = element.getAttribute('data-anchor')
+
+				document.getElementById(anchor).scrollIntoView({
+					behavior: 'smooth',
+					block: 'start'
+				}, 1000)
+
+				$('header .mob_menu_btn').removeClass('active')
+				$('body').removeClass('menu_open')
+				$('header .menu, header .phone').removeClass('show')
+			})
+		})
+	}
+
+
+	// Маска ввода
+	const phoneInputs = document.querySelectorAll('input[type=tel]')
+
+	if (phoneInputs) {
+		phoneInputs.forEach(el => {
+			IMask(el, {
+				mask: '+{7} (000) 000-00-00',
+				lazy: true
+			})
+		})
+	}
+
+
+	// Фокус при клике на название поля
+	const formLabels = document.querySelectorAll('form .label')
+
+	if (formLabels) {
+		formLabels.forEach(el => {
+			el.addEventListener('click', e => {
+				e.preventDefault()
+
+				el.closest('.line').querySelector('.input, textarea').focus()
+			})
+		})
+	}
+
+
+	// Моб. меню
+	$('header .mob_menu_btn').click((e) => {
+		e.preventDefault()
+
+		$('header .mob_menu_btn').toggleClass('active')
+		$('body').toggleClass('menu_open')
+		$('header .menu, header .phone').toggleClass('show')
+	})
+})
+
+
+
+window.addEventListener('resize', function () {
+	WH = window.innerHeight || document.clientHeight || document.getElementsByTagName('body')[0].clientHeight
+
+	let windowW = window.outerWidth
+
+	if (typeof WW !== 'undefined' && WW != windowW) {
+		// Перезапись ширины окна
+		WW = window.innerWidth || document.clientWidth || document.getElementsByTagName('body')[0].clientWidth
+
+
+		// Моб. версия
+		if (!fakeResize) {
+			fakeResize = true
+			fakeResize2 = false
+
+			document.getElementsByTagName('meta')['viewport'].content = 'width=device-width, initial-scale=1, maximum-scale=1'
+		}
+
+		if (!fakeResize2) {
+			fakeResize2 = true
+
+			if (windowW < 390) document.getElementsByTagName('meta')['viewport'].content = 'width=390, user-scalable=no'
+		} else {
+			fakeResize = false
+			fakeResize2 = true
+		}
+	}
+})
